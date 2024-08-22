0b7bc6b5846b6f1ea4e55cfd86dc292536ffeb12
Aynaz <aymzzii2019@gmail.com>
2024-08-22 15:19:12 +0430

    task-id-created

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@ const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
 const alertMessage = document.getElementById("alert-message");
-console.log(alertMessage)
 
 const todos = [];
+
+const generatedId = () => {
+    return Math.round(
+        Math.random() * Math.random() * Math.pow(10, 15)
+    ).toString();
+};
 
 const showAlert = (message, type) => {
     alertMessage.innerHTML = "";
@@ -16,24 +21,27 @@ const showAlert = (message, type) => {
     alertMessage.append(alert);
 
     setTimeout(() => {
-        alert.style.display = "none"
+        alert.style.display = "none";
     }, 2000)
-}
+};
+
+const 
 
 const addHandler = () => {
     const task = taskInput.value;
     const date = dateInput.value;
     const todo = {
-        task: task,
-        date: date,
+        id: generatedId(),
         completed: false,
+        task,
+        date,
     };
     if (task) {
         todos.push(todo);
         taskInput.value = "";
         dateInput.value = "";
         console.log(todos);
-        showAlert("Todo added successfully", "success")
+        showAlert("Todo added successfully", "success");
     } else {
         showAlert("please enter a todo!", "error");
     }
